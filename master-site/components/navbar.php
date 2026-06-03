12cc10b749b4ab61bb350b403caf9c44e77c8c67
S.Sarayoot <sarayoot_ball12@hotmail.com>
2026-06-03 15:36:17 +0700

    design: simplified to single page template

---
+++ master-site/components/navbar.php
@@ -0,0 +1,8 @@
+<!-- ตัวอย่างเมนูในหน้าเดียวกดแล้วเด้งลงไปหาเนื้อหา -->
+<ul class="navbar-nav ms-auto">
+    <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+    <li class="nav-item"><a class="nav-link" href="#guide">Investment Guide</a></li>
+    <li class="nav-item"><a class="nav-link" href="#faq">FAQs</a></li>
+    <li class="nav-item"><a class="nav-link" href="#blog">Whisky Blog</a></li>
+    <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+</ul>
