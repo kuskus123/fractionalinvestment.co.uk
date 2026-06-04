97d598a6647cfc9e42cb794551e66cf077c6b6a1
kuskusza123 <kuskusza@gmail.com>
2026-06-04 22:49:39 +0700

    ux: implement full-screen click target for the intro overlay explore triggering

--- master-site/index.php
+++ master-site/index.php
@@ -104,4 +104,4 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 </script>
 
-<?php require_once __DIR__ . '/components/footer.php'; ?
+<?php require_once __DIR__ . '/components/footer.php'; ?>
