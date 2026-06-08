e342ddf2cafa6123e83eeb8ebe6cd06b44e7a5f8
kuskusza123 <kuskusza@gmail.com>
2026-06-08 14:10:06 +0700

    feat: implement dynamic template architecture for guide page

--- master-site/config-system.php
+++ master-site/config-system.php
@@ -6,7 +6,7 @@
 
 $web = [
     'site_type'     => 'news',              // 📰 เลือกประเภทหน้าจอ: 'news' (ดึงข่าวนอกออโต้) หรือ 'blog' (เขียนบทความเอง)
-    'theme_class'   => 'theme-luxury-gold', // 🎨 เลือกธีมสี: 'theme-luxury-gold' หรือ 'theme-irish-emerald'
+    'theme_class'   => 'LUXURY ROSE PINK', // 🎨 เลือกธีมสี: 'theme-luxury-gold' หรือ 'theme-irish-emerald'
     'font_link'     => 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600&display=swap',
     'font_family'   => "'Cinzel', serif",
     'brand_name'    => "FAHMAI Hoding",
